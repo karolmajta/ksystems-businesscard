de12e8b38aa2293c8bdfd78f7fa87ce58c189a76
Karol Majta <karol@wpadne.pl>
2012-06-24 23:53:24 +0200

    Updated urls for tests

--- src/ks_bcard/tests/urls.py
+++ src/ks_bcard/tests/urls.py
@@ -1,7 +1,22 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from ..views import PageView
+from ks_bcard.views import PageView
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^(?P<username>\w+)/(?P<sitename>[-\w]+)/$', PageView.as_view(), name='site'),
+
+    url(r'^(?P<username>\w+)/(?P<sitename>[-\w]+)/(?P<path>[-\/\w]+)$', PageView.as_view(), name='page'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
